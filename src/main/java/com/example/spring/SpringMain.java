bbe618c18aa7f1b3b6c5dd426b88927082b4d3ac
Jony Liu <liu_zengqiang@126.com>
2019-11-05 19:53:17 +0800

    add spring rabbitmq

---
+++ src/main/java/com/example/spring/SpringMain.java
@@ -0,0 +1,17 @@
+package com.example.spring;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringMain {
+    public static void main(final String... args) throws Exception {
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "classpath:spring/rabbitmq-context.xml");
+        //RabbitMQ模板
+        RabbitTemplate template = ctx.getBean(RabbitTemplate.class);
+        //发送消息
+        template.convertAndSend("喵~");
+        Thread.sleep(1000);// 休眠1秒
+        ctx.destroy(); //容器销毁
+    }
+}
